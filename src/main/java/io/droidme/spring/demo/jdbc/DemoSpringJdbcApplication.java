ea258b4e88a1c5de86869234f463c4a901f9a1c0
Reinhard Merfeld <reinhard@merfeld.info>
2024-09-14 19:43:15 +0200

    inital commit

---
+++ src/main/java/io/droidme/spring/demo/jdbc/DemoSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package io.droidme.spring.demo.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/droidme/spring/demo/jdbc/DemoSpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.droidme.spring.demo.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
